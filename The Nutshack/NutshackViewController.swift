e98bb055cda7dc7bdc1c215e30a8aa8bf8d784b1
student8 <student8@jh-mba158-12.local>
2017-05-10 14:46:44 -0500

    Test

--- The Nutshack/NutshackViewController.swift
+++ The Nutshack/NutshackViewController.swift
@@ -9,27 +9,31 @@
 import UIKit
 
 class NutshackViewController: UIViewController {
+    var roids = 1
+    var model = 1
+    
+    @IBOutlet weak var redRoids: UIView!
+    
+    @IBOutlet weak var blueRoids: UIView!
+
+
+    var location = CGPoint(x: 0, y: 0)
     
     @IBOutlet weak var Person: UIImageView!
-    var roids = 1
     
-    
-    var location = CGPoint(x: 0, y: 0)
-
-    func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch : UITouch = touches.first as UITouch!
         location = touch.location(in: self.view)
         Person.center = location
     }
     
-    func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch : UITouch = touches.first as UITouch!
         location = touch.location(in: self.view)
         
         Person.center = location
-    
-    
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,13 +55,66 @@ class NutshackViewController: UIViewController {
             self.view.addSubview(Person)
         }
         
-        if roids == 4 { Person.frame = CGRect(x: Person.frame.origin.x, y: Person.frame.origin.y, width: 285, height: 30)
+        if roids == 4 { Person.frame = CGRect(x: Person.frame.origin.x, y: Person.frame.origin.y, width: 300, height: 50)
             self.view.addSubview(Person)
         }
         
         if roids == 5 {
-            roids = 1
-        }
+            Person.frame = CGRect(x: Person.frame.origin.x, y: Person.frame.origin.y, width: 85, height: 100)
+            self.view.addSubview(Person)
+        Person.image = UIImage(named:"Tito")
+            if roids == 3 {
+                roids = 1
+            }
+            
+            self.view.addSubview(blueRoids!)
+            self.view.addSubview(redRoids!)
+            
+            let animator = UIDynamicAnimator(referenceView: self.view)
+            
+            let gravity = UIGravityBehavior(items: [blueRoids!,
+                                                    redRoids!])
+            let vector = CGVector(dx: 0.0, dy: 1.0)
+            gravity.gravityDirection = vector
+            
+            let collision = UICollisionBehavior(items: [blueRoids!,
+                                                        redRoids!])
+            collision.translatesReferenceBoundsIntoBoundary = true
+            
+            animator.addBehavior(collision)
+            animator.addBehavior(gravity)
+            
+            func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+                
+                if let theTouch = touches.first {
+                    var currentLocation = theTouch.location(in: self.view)
+                    let offset = UIOffsetMake(20, 20)
+                    let attachment = UIAttachmentBehavior(item: blueRoids!,
+                    offsetFromCenter: offset,
+                    attachedToAnchor: currentLocation)
+                    animator.addBehavior(attachment)
+                    
+                    func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+                        if let theTouch = touches.first {
+                            
+                            attachment.anchorPoint = currentLocation
+                            
+                            func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+                                animator.removeBehavior(attachment)                }
+                
+            }
+            
 
-        }
-    }
+            
+            }
+                }
+
+                }
+                }
+                }
+    
+    
+                }
+
+
+                
